586ec0aa63a36ca50e35c905058b04bf35feb483
soapwhir9licloudcom <soap.whir_9l@icloud.com>
2025-05-31 18:24:28 -0400

    Update token.js

--- token.js
+++ token.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const { AccessToken } = require('twilio').jwt;
+const VoiceResponse = require('twilio').twiml.VoiceResponse;
 const VoiceGrant = AccessToken.VoiceGrant;
 
 const app = express();
 app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,9 +16,9 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const apiKeySid = process.env.TWILIO_API_KEY;
 const apiKeySecret = process.env.TWILIO_API_SECRET;
 const twimlAppSid = process.env.TWIML_APP_SID;
+const CALLER_ID = process.env.CALLER_ID || '+16144120343'; // <- This should be Number C
 
-
-
+// ====== /token endpoint ======
 app.get('/token', (req, res) => {
   const identity = 'FleetFlowUser_' + Math.floor(Math.random() * 10000);
 
@@ -28,6 +31,7 @@ app.get('/token', (req, res) => {
     identity: identity,
     ttl: 3600,
   });
+
   token.addGrant(voiceGrant);
 
   res.json({
@@ -36,6 +40,31 @@ app.get('/token', (req, res) => {
   });
 });
 
+// ====== /voice endpoint (Twilio webhook for incoming calls) ======
+app.post('/voice', (req, res) => {
+  const twiml = new VoiceResponse();
+
+  const dial = twiml.dial({ callerId: CALLER_ID });
+
+  // If calling a client, use <Client>, otherwise <Number>
+  const to = req.body.To;
+
+  if (to) {
+    if (/^[\d\+\-\(\) ]+$/.test(to)) {
+      dial.number(to);
+    } else {
+      dial.client(to);
+    }
+  } else {
+    twiml.say("Thanks for calling FleetFlow. Please hold while we connect you.");
+    dial.client('FleetFlowUser_9351');
+  }
+
+  res.type('text/xml');
+  res.send(twiml.toString());
+});
+
+// ====== Start server ======
 app.listen(PORT, () => {
-  console.log(`Token server running on port ${PORT}`);
+  console.log(`FleetFlow Token & Voice server running on port ${PORT}`);
 });
